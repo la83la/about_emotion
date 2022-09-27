758557b6a9a7ff126c7a4f14c5d20f5fa6f6dbea
DESKTOP-QLIHU4O\USER <whwen0528@gmail.com>
2022-09-27 23:20:49 +0800

    add new dataset, delete all old files

--- moodscores/helper_functions.py
+++
@@ -1,9 +0,0 @@
-from nltk.tokenize import TweetTokenizer
-
-def tokenizer(tweet, case = 'lower'):
-    tknzr = TweetTokenizer()
-    if case == 'lower': 
-    	return tknzr.tokenize(tweet.lower())
-    else: 
-    	tknzr.tokenize(tweet)
-    
